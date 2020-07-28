69f8b128d95f17fa3a9277e4b091d8b9168c6c94
Kunaal Sharma <kss45@duke.edu>
2020-07-28 00:07:48 -0700

    updated paths and created cook slave

--- cook_slave.py
+++ cook_slave.py
@@ -11,83 +11,20 @@ clayInterfacePath = "Cooking/clayInterface.png"
 cookInterfacePath = "Cooking/cookInterface.png"
 
 portableKeyPath = "Cooking/portableKey.png"
-portableClickInterfacePath = "portableClickInterface.png"
-extendInterfacePath = "extendInterface.png"
+portableClickInterfacePath = "Cooking/portableClickInterface.png"
+extendInterfacePath = "Cooking/extendInterface.png"
 
 PRESET_KEY = '1'
 PORTABLE_PRESET = '2'
 SILVERHAWK_KEY = 'q'
 
-def extendPortable(portableKeyCoordinates, extendInterfaceLocation, tries = 0):
-	(x, y, xT, yT) = portableKeyCoordinates
-	(x, y, xT, yT) = (5 + x//2, 5 + y//2, -5 + xT//2, -5 + yT//2)
-
-	if tries == 10:
-		print("Failed to extend portable")
-		return False
-	
-	def moveToPortableKey():
-		mouse.moveRect(x, y, xT, yT)
-
-		sleepDuration = -1
-		while sleepDuration < 0:
-			sleepDuration = np.random.normal(0.2, 0.01)
-		sleep(sleepDuration)
-
-		pyautogui.click(button='right')
-
-	portableClickLocation = utils.Location()
-
-	succ = utils.performAction(moveToPortableKey, 
-		utils.makeSuccess(portableClickLocation, portableClickInterfacePath, False))
-	if not succ:
-		print("Failed to right click portable key, retrying")
-		extendPortable(portableKeyCoordinates, extendInterfaceLocation, tries = tries + 1)
-		
-
-	sleep(3)
-
-	(x, y, xT, yT) = utils.convert(portableClickLocation.getLoc())
-	(x, y, xT, yT) = (x//2, y//2, xT//2, yT//2)
-	mouse.moveRect(x, y, xT, yT, teleportRandom=False)
-	pyautogui.click()
-	
-	sleep(3)
-
-	if not utils.checkLocation(extendInterfaceLocation, extendInterfacePath):
-		print("Failed to open extend interface, retrying")
-		extendPortable(portableKeyCoordinates, extendInterfaceLocation, tries = tries + 1)
-
-	sleepDuration = -1
-	while sleepDuration < 0:
-		sleepDuration = np.random.normal(0.2, 0.2)
-	utils.spamPress('1')
-	sleep(sleepDuration)
-	utils.spamPress('enter')
-
-	sleep(3)
-
-	if utils.checkLocation(extendInterfaceLocation, extendInterfacePath):
-		print("Failed to extend portable, retrying")
-		extendPortable(portableKeyCoordinates, extendInterfaceLocation, tries = tries + 1)
-
-	return True
-
 def runBot():
-	extendInterfaceLocation = utils.Location()
 	bankLocation = utils.Location()
 	clayInterfaceLocation = utils.Location()
 	cookInterfaceLocation = utils.Location()
-	silverhawkLocation = utils.Location()
-	portableKeyCoordinates = None
-
-	sleep(2)
-	portableKeyCoordinates = utils.convert(pyautogui.locateOnScreen(portableKeyPath))
-	print(portableKeyCoordinates)
 
 	rangeCoordinates = utils.getBoundingRectangle()
 	bankCoordinates = utils.getBoundingRectangle()
-	silverhawkCoordinates = utils.getBoundingRectangle()
 
 	def openBank():
 		x, y, xT, yT = bankCoordinates
@@ -99,46 +36,9 @@ def runBot():
 		mouse.moveRect(x, y, xT, yT)
 		pyautogui.click()
 
-	def extendSilverhawk():
-		x, y, xT, yT = silverhawkCoordinates
-		mouse.moveRect(x, y, xT, yT)
-		pyautogui.click()
-
-	succ = extendPortable(portableKeyCoordinates, extendInterfaceLocation)
-	if not succ:
-		return 
-	sleep(2)
-	utils.performAction(extendSilverhawk, lambda : True)
-	sleep(2)
-
-	portableTimer = time()
 	startTime = time()
 
 	while True:
-
-		if time() - portableTimer > 50 * 60:
-			succ = utils.performAction(openBank, 
-			utils.makeSuccess(bankLocation, bankInterfacePath, False))
-			if not succ:
-				print("Failed to open bank")
-				return False
-
-			sleep(3)
-
-			succ = utils.performAction(utils.makeKeyPressAction(PORTABLE_PRESET), 
-				utils.makeSuccess(bankLocation, bankInterfacePath, True))
-			if not succ:
-				print("Failed to withdraw preset")
-				return False
-
-			succ = extendPortable(portableKeyCoordinates, extendInterfaceLocation)
-			if not succ:
-				return
-			sleep(2)
-			utils.performAction(extendSilverhawk, lambda : True)
-			sleep(2)
-
-			portableTimer = time() - 30
 
 		if time() - startTime > 16 * 3600:
 			print("Returning after finishing")
